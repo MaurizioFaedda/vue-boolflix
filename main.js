0499f87fc253ee0ab9e5c922abc2d63f7fdf44ef
Maurizio <maurizio.faedda1@gmail.com>
2020-11-24 16:10:55 +0100

    Stampati info film-fatto da zero senza bootstrap

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 var app = new Vue ({
     el: '#root',
     data: {
+        films: [],
+        query: ''
 
+    },
+
+    methods: {
+        utent_search(query){
+
+            axios.get('https://api.themoviedb.org/3/search/movie', {
+                params: {
+                    api_key: '04b3ed8850d2c7627102f2c8b2457428',
+                    query: query
+                }
+            })
+            .then((res) => {
+                this.films = res.data.results;
+            })
+            this.query = '';
+        }
     }
+
 
 })
